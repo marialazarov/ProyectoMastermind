5f18261439ca8fd72e90df25f4e21a548c02d2f5
MacBooK Pro <holamarialazaro@gmail.com>
2023-11-28 22:11:31 +0100

    Validación colores

--- js/pantalla2.js
+++ js/pantalla2.js
@@ -1,19 +1,24 @@
 document.getElementById('botonlisto').addEventListener('click', function () {
-    var coloresSeleccionados = []
-    // se crea el array para uso posterior
+    var coloresSeleccionados = [];
 
-
+    // Obtener los valores de los colores seleccionados por el usuario
     coloresSeleccionados.push(document.querySelector('.color1').value);
     coloresSeleccionados.push(document.querySelector('.color2').value);
     coloresSeleccionados.push(document.querySelector('.color3').value);
     coloresSeleccionados.push(document.querySelector('.color4').value);
-    // se almacenan en ese array los valores de los colores que elija el usuario
-   
-   
-    localStorage.setItem('coloresSeleccionados', JSON.stringify(coloresSeleccionados));
-    //propiedad para almacenarlos en la información del navegador para su uso posterior.
-   
-    window.location.href ="juego.html";
-    //al pulsar el botón 'listo' almacena toda esta información previa y además nos
-    //redirige a la próxima pantalla 'juego'.
+
+    // Verificar si algún color está vacío o tiene el valor predeterminado
+    if (coloresSeleccionados.some(color => color.trim() === '' || color.trim() === 'default')) {
+        // Mostrar una alerta si hay algún color vacío o no seleccionado
+        alert('Por favor, elija un color diferente para cada posición.');
+    } else if (new Set(coloresSeleccionados).size !== coloresSeleccionados.length) {
+        // Mostrar una alerta si hay colores duplicados
+        alert('Por favor, elija un color diferente para cada posición.');
+    } else {
+        // Almacenar los colores seleccionados en el localStorage
+        localStorage.setItem('coloresSeleccionados', JSON.stringify(coloresSeleccionados));
+
+        // Redirigir a la próxima pantalla 'juego'
+        window.location.href = "juego.html";
+    }
 });
