855ddbdd42e9826d4665230b81f2e4d726cf89cd
MacBooK Pro <holamarialazaro@gmail.com>
2023-11-28 22:19:43 +0100

    Validacion usuario

--- js/pantalla1.js
+++ js/pantalla1.js
@@ -1,13 +1,19 @@
 let botonprincipiante = document.getElementById('botonprincipiante');
 
-
 botonprincipiante.addEventListener('click', function () {
-    
+    // Obtiene el valor del nombre de usuario
     let nombreUsuario = document.getElementById('input1').value;
+
+    // Verificar si el nombre de usuario está vacío
+    if (!nombreUsuario.trim()) {
+        // Muestra una alerta si el nombre de usuario está vacío
+        alert('Por favor, ingresa tu nombre de usuario');
+        return; // Detén la ejecución si el nombre de usuario está vacío
+    }
 
     // Guarda el nombre de usuario en localStorage para que esté disponible en otras páginas
     localStorage.setItem('nombreUsuario', nombreUsuario);
 
     // Redirige a la otra página
     window.location.href = 'pantalla2.html';
-    });
+});
